5884d7c49db409ac8db57d99141700b9102cb3d8
D.Poriya <dharmeshkporiya@gmail.com>
2022-06-17 16:59:56 +0530

    code of Binary Tree Cameras

---
+++ 01_LeetCode Questions/Hard/0968_Binary Tree Cameras/Solution-01/0968_Binary_Tree_Cameras.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    int cameras;
+    int isCameraNeeded(TreeNode* root){
+        if(nullptr==root) return 1;
+        
+        int left = isCameraNeeded(root->left);
+        int right = isCameraNeeded(root->right);
+        
+        if(-1==left || -1==right){
+            cameras ++;
+            return 0;
+        }
+        if(0==left || 0==right){
+            return 1;
+        }
+        return -1;
+    }
+public:
+    int minCameraCover(TreeNode* root) {
+        this->cameras = 0;
+        
+        if(isCameraNeeded(root)==-1) cameras ++;
+        
+        return cameras;
+    }
+};
